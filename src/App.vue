490931281049b6f095182269032eeabe3969114b
Charon <2662687548@qq.com>
2021-03-05 10:14:17 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div>hello docker</div>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    6ccccc222
+    6ccccc222333333333
   </div>
 </template>
 
